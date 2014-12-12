0ac56deca48d6570c2a6062d7a66e681174567ac
Maciek Paliński <maciekpalinski@gmail.com>
2014-12-12 13:47:55 +0000

    vcr config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  VCR.configure do |c|
+    c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+    c.hook_into :webmock
+  end
 end
