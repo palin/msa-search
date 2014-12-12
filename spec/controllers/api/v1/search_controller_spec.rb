514a8204f166fe0e3390b8177c42a5fafecce00c
Maciek Paliński <maciekpalinski@gmail.com>
2014-12-12 10:20:42 +0000

    search controller - action, view and working spec

--- spec/controllers/api/v1/search_controller_spec.rb
+++ spec/controllers/api/v1/search_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 describe Api::V1::SearchController do
+
+  describe "GET index" do
+    subject { get :index, format: :json }
+
+    its(:status) { should == 200 }
+  end
 end
